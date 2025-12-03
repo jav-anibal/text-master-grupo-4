bae6828fdd460c5491f143707ee056caa81314c3
anibaL <a88anibal@gmail.com>
2025-12-03 19:55:40 +0100

    Setup -  inicial -  proyecto node.js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use(morgan('combined'));
+app.use(express.json());
+
+// Ruta temporal para verificar que funciona
+app.get('/', (req, res) => {
+  res.json({ message: 'TextMaster API - Configuración inicial' });
+});
+
+module.exports = app;
